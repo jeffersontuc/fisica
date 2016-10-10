19bc5173f55a91362b01bc32fde1c643d478746e
Jefferson Carlos Benedito <jefiim.ad@gmail.com>
2016-10-10 06:53:08 -0300

    Questão 3 finalizada

--- MyApp.js
+++ MyApp.js
@@ -20,6 +20,16 @@ angular.module('fisica', ['ngMaterial'])
 
 
 
+
+    $scope.velocidade;
+    $scope.campo;
+    $scope.carga;
+    $scope.angulo;
+    $scope.expm;
+    $scope.magnetica;
+
+
+
     $scope.calculaForca = function(){
     	var expRes = parseInt($scope.exp2) + parseInt($scope.exp1);
     	$scope.result = parseFloat($scope.k) * parseFloat($scope.q1) * parseFloat($scope.q2) / Math.pow(parseFloat($scope.dist), 2);
@@ -29,13 +39,18 @@ angular.module('fisica', ['ngMaterial'])
     		expRes = 'x10^' + expRes + " N";
     	}
     	$scope.result += expRes;
-    	stringResult = procuraZero(JSON.stringify($scope.result));
     };
 
     $scope.calculaEnergia = function(){
     	$scope.energia = Math.pow(parseFloat($scope.eletromotriz), 2) * parseFloat($scope.tempo) * 60 / (parseFloat($scope.rfio) + parseFloat($scope.rbateria));
     	$scope.energia += " J";
     };
+
+
+    $scope.calculaForcaMagnetica = function(){
+    	$scope.magnetica = parseFloat($scope.carga) * parseFloat($scope.velocidade) * parseFloat($scope.campo) * Math.sin(parseFloat($scope.angulo) * (Math.PI / 180));
+    	$scope.magnetica += 'x10^' + $scope.expm + " N";
+    }
 
     $scope.currentNavItem = 'page1';
 });
